7abdf722e3aab7444e42c48809f86844f829baa6
LaudaDev <goran375@yahoo.com>
2016-03-23 21:04:14 +0100

    * Cleanup

--- src/main/java/activiti/spring/javnaNabavka/springweb/controllers/ApplicationController.java
+++ src/main/java/activiti/spring/javnaNabavka/springweb/controllers/ApplicationController.java
@@ -67,9 +67,6 @@ public class ApplicationController {
 
 		List<Task> candidateTasks = taskService.createTaskQuery().taskCandidateUser(id).list();
 		model.addAttribute("candidateTasks", candidateTasks);
-		
-		historyService.createHistoricDetailQuery();
-	
 
 		return "application/tasksList";
 
